77548c6818712da11793c7d6092089c86ed2f517
Abdullahazeem2 <abdullahazeemkhi@gmail.com>
2026-07-01 23:21:58 +0500

    1 page complete

---
+++ app.js
@@ -0,0 +1,14 @@
+// console.log("js running------>")
+
+const menu = document.querySelectorAll(".menu");
+
+// console.log(menu);
+
+menu.forEach((item) => {
+    item.addEventListener("click", () =>{
+      window.location.href = "Menu/index.html";
+    })
+        
+    
+        
+    })
