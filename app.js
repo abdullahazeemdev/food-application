4f7368af666982f9fe38e409a50b25ed4aa1e5a8
Abdullahazeem2 <abdullahazeemkhi@gmail.com>
2026-07-05 16:17:40 +0500

    about page contact page done

--- app.js
+++ app.js
@@ -1,13 +1,43 @@
 // console.log("js running------>")
 
-const menu = document.getElementById("menu")
+const menu = document.getElementById("menu");
+const sign = document.getElementById("sign-up")
+const login = document.getElementById("login")
+// const about = document.getElementById("about")
+// const contact = document.getElementById("contact")
+// // console.log(contact,sign,menu,login,about)
 
-// console.log(menu);
+// // console.log(sign);
+
 
 menu.addEventListener("click", () =>{
-      window.location.href = "Menu/index.html";
+       window.location.href = "Menu/index.html";
 })
-        
-    
-      
 
+sign.addEventListener("click", () =>{
+       window.location.href = "Signup/signup.html";
+})
+    
+login.addEventListener("click" , () => {
+      window.location.href = "login/index.html";
+})
+
+// about.addEventListener("click", () => {
+//       console.log(about)
+//       // window.location.href = "about/index.html";
+// })
+
+// contact.addEventListener("click", () => {
+//       window.location.href = "contact/index.html";
+// })
+
+
+// menu.forEach(btn => {
+
+//     btn.addEventListener("click", () => {
+
+//         window.location.href = "Menu/index.html";
+
+//     });
+
+// });
